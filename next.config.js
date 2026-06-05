f5a226fbcce23de3d989f697a7db80c697723742
Souparna Kundu <kundusouparna@gmail.com>
2026-06-05 14:03:11 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+
+  basePath: '/Souparna_Portfolio',
+  assetPrefix: '/Souparna_Portfolio',
+
   images: {
     unoptimized: true,
   },
+
   trailingSlash: true,
 }
 
